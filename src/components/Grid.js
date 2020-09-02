f2d75222986ad5f9bb729390862132b22cdac5c6
robinhoets <robertets88@gmail.com>
2020-09-02 19:13:48 -0400

    button/funciton to change colors

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,29 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Grid.css'
 
-export default function Grid(){
+export default class Grid extends Component {
 
-	return(
-		<>
-			<div className="flex-container">
-				<div className="square">HELLO</div>
-				<div className="square">HELLO</div>
-				<div className="square">HELLO</div>
-				<div className="square">HELLO</div>
-				<div className="square">HELLO</div>
-				<div className="square">HELLO</div>
-				<div className="square">HELLO</div>
-				<div className="square">HELLO</div>
-				<div className="square">HELLO</div>
-				<div className="square">HELLO</div>
-				<div className="square">HELLO</div>
-				<div className="square">HELLO</div>
-				<div className="square">HELLO</div>
-				<div className="square">HELLO</div>
-				<div className="square">HELLO</div>
-				<div className="square">HELLO</div>
-			</div>
-		</>
-		)
+	constructor(){ 
+		super();
+	    this.state={
+	      color : '#00BCD4'	 
+	    }
+  	}
 
+	changeColor = () => {
+		var ColorCode = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + 
+						   ',' + (Math.floor(Math.random() * 256)) + ')';
+		this.setState({color:ColorCode})
+	}
+
+	render() {
+			return(
+				<div>
+					<button onClick={()=> this.changeColor()}> change me </button>
+					<div className="flex-container">
+						<div className="square" style={{backgroundColor:this.state.color}}>HELLO</div>
+						<div className="square">HELLO</div>
+						<div className="square">HELLO</div>
+						<div className="square">HELLO</div>
+						<div className="square">HELLO</div>
+						<div className="square">HELLO</div>
+						<div className="square">HELLO</div>
+						<div className="square">HELLO</div>
+						<div className="square">HELLO</div>
+						<div className="square">HELLO</div>
+						<div className="square">HELLO</div>
+						<div className="square">HELLO</div>
+						<div className="square">HELLO</div>
+						<div className="square">HELLO</div>
+						<div className="square">HELLO</div>
+						<div className="square">HELLO</div>
+					</div>
+				</div>
+			);		
+	}
 }
