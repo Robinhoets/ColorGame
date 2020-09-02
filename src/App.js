3b7e5f886d0b42001cebef01565857ad1d42f17d
robinhoets <robertets88@gmail.com>
2020-09-01 21:42:57 -0400

    added Grid component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Grid from './components/Grid.js'
 
 function App() {
   return (
     <div>
-      Hello World
+      <Grid />
     </div>
   );
 }
---
+++ src/components/Grid.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Grid(){
+
+	return(
+		<div>
+			Hello World
+		</div>
+		)
+
+}
