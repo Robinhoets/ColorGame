5b9b337e9a2124bd1e1ef4b0ab0f5ca49d0b1d8a
robinhoets <robertets88@gmail.com>
2020-09-02 18:34:18 -0400

    created css & grid squares

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import './Grid.css'
 
 export default function Grid(){
 
 	return(
-		<div>
-			Hello World
-		</div>
+		<>
+			<div className="flex-container">
+				<div className="square">HELLO</div>
+				<div className="square">HELLO</div>
+				<div className="square">HELLO</div>
+				<div className="square">HELLO</div>
+				<div className="square">HELLO</div>
+				<div className="square">HELLO</div>
+				<div className="square">HELLO</div>
+				<div className="square">HELLO</div>
+				<div className="square">HELLO</div>
+				<div className="square">HELLO</div>
+				<div className="square">HELLO</div>
+				<div className="square">HELLO</div>
+				<div className="square">HELLO</div>
+				<div className="square">HELLO</div>
+				<div className="square">HELLO</div>
+				<div className="square">HELLO</div>
+			</div>
+		</>
 		)
 
 }
